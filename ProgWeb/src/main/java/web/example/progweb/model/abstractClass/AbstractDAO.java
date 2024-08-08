6fe0da5ba58ecb01ebf1d4cee31cd389976fe7c3
Daniele Ye <daniele.ye03@gmail.com>
2024-08-08 16:44:14 +0200

    Initial commit

---
+++ ProgWeb/src/main/java/web/example/progweb/model/abstractClass/AbstractDAO.java
@@ -0,0 +1,44 @@
+package web.example.progweb.model.abstractClass;
+
+import java.sql.*;
+
+public abstract class AbstractDAO {
+    protected Connection connection;
+    protected Statement statement;
+
+    public AbstractDAO() throws SQLException, ClassNotFoundException {
+        String url = "jdbc:derby://localhost:1527/ProgWebDB";
+        String user = "App";
+        String password = "App";
+        Class.forName("org.apache.derby.jdbc.ClientDriver"); // Caricamento del driver
+        connection = DriverManager.getConnection(url, user, password); // Connessione al database
+        statement = connection.createStatement();
+    }
+
+    public AbstractDAO (Connection connection) throws SQLException {
+        this.connection = connection;
+        statement = connection.createStatement();
+    }
+
+    public ResultSet unsafeExecuteQuery(String query) throws SQLException {
+        return statement.executeQuery(query);
+    }
+
+    public void close() throws SQLException {
+        connection.close();
+    }
+
+//  expect input in the format dd/mm/yyyy and return in the format yyyy-mm-dd
+    public String formatDate(String date) {
+        String[] dateParts = date.split("/");
+        return dateParts[2] + "-" + dateParts[1] + "-" + dateParts[0];
+    }
+
+//  expect input in the format dd/mm/yyyy hh:mm:ss and return in the format yyyy-mm-dd hh:mm:ss
+    public String formatDateTime(String date) {
+        String[] dateParts = date.split(" ");
+        String[] dateParts2 = dateParts[0].split("/");
+        return dateParts2[2] + "-" + dateParts2[1] + "-" + dateParts2[0] + " " + dateParts[1];
+    }
+
+}
--- ProgWeb/src/main/java/web/example/progweb/model/model.java
+++
@@ -1,4 +0,0 @@
-package web.example.progweb.model;
-
-public class model {
-}
